0ccbb47fc1411006941d77d61f468206d09e0d76
Leandro <115833497+leandrogarcia7@users.noreply.github.com>
2025-11-14 20:00:54 -0500

    Refine responsive layout for group, breed and client views

--- interfaces/uiclientes.php
+++ interfaces/uiclientes.php
@@ -1,56 +1,91 @@
 <?php
+require '../negocio/USUARIO.php';
 require '../negocio/CLIENTE.php';
-$usu= new USUARIO();
-   session_start();
 
+$usu = new USUARIO();
+$obj = new CLIENTE();
 
+session_start();
 
-$obj=new CLIENTE();
-require_once("../encabezado.php"); 
-?>
-
-<html lang="es">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
- <script src="../js/script.js"></script>   <body>
-   <div class="banner">
-        <img src="../img/rrhh.png" alt="Banner de Animales">
-    </div>  
-<?php
-
-//echo "<table style='width:90%'><td style='width:40%'>";
-if(isset($_SESSION['idhac'])){
-     $usu-> mostrarMenu(1,1);
-}else{
-  header("Location: ../login.php");
-exit;
+if (!isset($_SESSION['idhac'])) {
+    header("Location: ../login.php");
+    exit;
 }
 
+require_once("../encabezado.php");
 
-//echo "</td><td>";
-//mostrar las opciones de raza en botones animados
-//$empleados->mostrarInicio();
-//mostrar Crear Raza
-
-//btteliasi
-//IF (isset($_REQUEST['btteliasi'])){    $grupo->eliminarasignarAnimalGrupo($_REQUEST['btteliasi']);$grupo->mostrarModificar($_REQUEST['idgru']);}
-
-
-//IF (isset($_REQUEST['bttasigru'])){    $grupo->asignarAnimalGrupo($_REQUEST['idgru'],$_REQUEST['idani']);$grupo->mostrarModificar($_REQUEST['idgru']);}
-
-IF (isset($_REQUEST['bttcrear'])){    $obj->mostrarCrear();}
-//guardar los datos del nuevo
-IF (isset($_REQUEST['bttnuevo'])){    $obj->nuevo($_REQUEST);}
-//mostrar listado de razas
-IF (isset($_REQUEST['bttbuscar'])){    $obj->buscar($_REQUEST['txtbuscar']);}
-//mostrar para modificar
-IF (isset($_REQUEST['bttsel'])){    $obj->mostrarModificar($_REQUEST['bttsel']);}
-//guardar los cambios en el modificar
-IF (isset($_REQUEST['bttmod'])){    $obj->modificarCliente($_REQUEST);}
-//eiminar los datos
-IF (isset($_REQUEST['btteli'])){    $obj->eliminarCliente($_REQUEST['btteli']);}
-
-$obj->mostrarInicio();
 ?>
-        </body>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gestión de clientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet" type="text/css"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../js/script.js"></script>
+</head>
+<body class="bg-light">
+<main class="container-fluid py-4">
+    <div class="row justify-content-center mb-4">
+        <div class="col-12 col-lg-10">
+            <div class="card shadow-sm border-0">
+                <div class="card-body text-center text-md-start">
+                    <figure class="banner mb-3">
+                        <img class="img-fluid rounded-3" src="../img/rrhh.png" alt="Banner de clientes">
+                    </figure>
+                    <h1 class="titulointerface h3 mb-0">Gestión de clientes</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row g-4 justify-content-center">
+        <aside class="col-12 col-lg-3">
+            <div class="card shadow-sm h-100 border-0">
+                <div class="card-body p-3">
+                    <?php
+                    if (isset($_SESSION['idhac'])) {
+                        $usu->mostrarMenu(1, 1);
+                    }
+                    ?>
+                </div>
+            </div>
+        </aside>
+        <section class="col-12 col-lg-7 col-xl-6">
+            <div class="card shadow-sm border-0">
+                <div class="card-body">
+                    <?php
+                    if (isset($_REQUEST['bttcrear'])) {
+                        $obj->mostrarCrear();
+                    }
+
+                    if (isset($_REQUEST['bttnuevo'])) {
+                        $obj->nuevo($_REQUEST);
+                    }
+
+                    if (isset($_REQUEST['bttbuscar'])) {
+                        $obj->buscar($_REQUEST['txtbuscar']);
+                    }
+
+                    if (isset($_REQUEST['bttsel'])) {
+                        $obj->mostrarModificar($_REQUEST['bttsel']);
+                    }
+
+                    if (isset($_REQUEST['bttmod'])) {
+                        $obj->modificarCliente($_REQUEST);
+                    }
+
+                    if (isset($_REQUEST['btteli'])) {
+                        $obj->eliminarCliente($_REQUEST['btteli']);
+                    }
+
+                    $obj->mostrarInicio();
+                    ?>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+</body>
 </html>
